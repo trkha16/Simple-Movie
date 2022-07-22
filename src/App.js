02ac734ac2f0fcaf508258b2bf29e54962f341e6
trkha16 <tbkha1609@gmail.com>
2022-07-23 01:09:49 +0700

    Update UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,116 @@
+import { Fragment } from "react";
+import { NavLink } from "react-router-dom";
+
 function App() {
-    return <div></div>;
+    return (
+        <Fragment>
+            <header className="header flex items-center justify-center gap-x-5 text-white py-10 mb-10">
+                <span className="text-primary">Home</span>
+                <span>Movies</span>
+            </header>
+            <section className="banner h-[500px] page-container mb-20">
+                <div className="w-full h-full rounded-lg relative">
+                    <div className="overlay absolute inset-0 bg-gradient-to-t from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0.5)] rounded-lg"></div>
+                    <img
+                        src="https://cdnimg.vietnamplus.vn/t1200/Uploaded/Mtpyelagtpy/2019_04_29/avengersendgame2904.jpg"
+                        alt=""
+                        className="w-full h-full object-cover rounded-lg"
+                    />
+                    <div className="absolute left-5 bottom-5 w-full text-white">
+                        <h2 className="font-bold text-3xl mb-5">
+                            Avengers: Endgame
+                        </h2>
+                        <div className="flex items-center gap-x-3 mb-8">
+                            <span className="py-2 px-4 border border-white rounded-md">
+                                Adventure
+                            </span>
+                            <span className="py-2 px-4 border border-white rounded-md">
+                                Adventure
+                            </span>
+                            <span className="py-2 px-4 border border-white rounded-md">
+                                Adventure
+                            </span>
+                        </div>
+                        <button className="py-3 px-6 rounded-lg bg-primary text-white font-mediums">
+                            Watch
+                        </button>
+                    </div>
+                </div>
+            </section>
+            <section className="movies-layout page-container pb-20">
+                <h2 className="text-white capitalize mb-10 text-3xl font-bold">
+                    now playing
+                </h2>
+                <div className="movie-list grid grid-cols-4 gap-10">
+                    <div className="movie-card rounded-lg p-3 bg-slate-800 text-white">
+                        <img
+                            src="https://cdnimg.vietnamplus.vn/t1200/Uploaded/Mtpyelagtpy/2019_04_29/avengersendgame2904.jpg"
+                            alt=""
+                            className="w-full h-[250px] object-cover rounded-lg mb-5"
+                        />
+                        <h3 className="text-white text-xl font-bold mb-3">
+                            Spiderman
+                        </h3>
+                        <div className="flex items-center justify-between text-sm opacity-50 mb-10">
+                            <span>2017</span>
+                            <span>7.4</span>
+                        </div>
+                        <button className="py-3 px-6 rounded-lg capitalize bg-primary w-full">
+                            Watch now
+                        </button>
+                    </div>
+                </div>
+            </section>
+            <section className="movies-layout page-container pb-20">
+                <h2 className="text-white capitalize mb-10 text-3xl font-bold">
+                    top rated
+                </h2>
+                <div className="movie-list grid grid-cols-4 gap-10">
+                    <div className="movie-card rounded-lg p-3 bg-slate-800 text-white">
+                        <img
+                            src="https://cdnimg.vietnamplus.vn/t1200/Uploaded/Mtpyelagtpy/2019_04_29/avengersendgame2904.jpg"
+                            alt=""
+                            className="w-full h-[250px] object-cover rounded-lg mb-5"
+                        />
+                        <h3 className="text-white text-xl font-bold mb-3">
+                            Spiderman
+                        </h3>
+                        <div className="flex items-center justify-between text-sm opacity-50 mb-10">
+                            <span>2017</span>
+                            <span>7.4</span>
+                        </div>
+                        <button className="py-3 px-6 rounded-lg capitalize bg-primary w-full">
+                            Watch now
+                        </button>
+                    </div>
+                </div>
+            </section>
+            <section className="movies-layout page-container pb-20">
+                <h2 className="text-white capitalize mb-10 text-3xl font-bold">
+                    trending
+                </h2>
+                <div className="movie-list grid grid-cols-4 gap-10">
+                    <div className="movie-card rounded-lg p-3 bg-slate-800 text-white">
+                        <img
+                            src="https://cdnimg.vietnamplus.vn/t1200/Uploaded/Mtpyelagtpy/2019_04_29/avengersendgame2904.jpg"
+                            alt=""
+                            className="w-full h-[250px] object-cover rounded-lg mb-5"
+                        />
+                        <h3 className="text-white text-xl font-bold mb-3">
+                            Spiderman
+                        </h3>
+                        <div className="flex items-center justify-between text-sm opacity-50 mb-10">
+                            <span>2017</span>
+                            <span>7.4</span>
+                        </div>
+                        <button className="py-3 px-6 rounded-lg capitalize bg-primary w-full">
+                            Watch now
+                        </button>
+                    </div>
+                </div>
+            </section>
+        </Fragment>
+    );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                body: ["DM Sans", "sans-serif"],
+            },
+            colors: {
+                primary: "#F62682",
+            },
+        },
     },
     plugins: [],
 };
